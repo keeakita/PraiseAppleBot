935b9bfc0c731f5ce4f2429c6e484161aad3ca21
William Osler <william@oslers.us>
2016-01-31 01:41:55 -0500

    Initial Commit

---
+++ PraiseAppleBot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PraiseAppleBot
+//
+//  Created by William Osler on 1/31/16.
+//  Copyright © 2016 William Osler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
